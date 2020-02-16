9b6798b1e11dfe79454889385d6b6753174ca8de
Jamison Engels <engelsj@tcd.ie>
2020-02-16 12:40:06 +0000

    Worked a bit more on the assignment

--- Assignment-1/assignment1.pl
+++ Assignment-1/assignment1.pl
@@ -30,7 +30,6 @@ less-than([[Node1|_],Cost1],[[Node2|_],Cost2]) :- heuristic(Node1,Hvalue1), heur
 
 
 heuristic(Node, Heuristic) :- length(Node, Heuristic).
-
 arc([H|T],Node,Cost,KB) :- member([H|B],KB), append(B,T,Node),
                            length(B,L), Cost is L+1.
 
@@ -40,7 +39,29 @@ astar(Node,Path,Cost) :- kb(KB), astar(Node,Path,Cost,KB).
 
 % Written Work 
 
-astar(Node, Path, Cost, KB) :- findRoute(Node, Node,)
+astar(Node,Path,Cost,KB) :- search([[Node,0,[Node]]], Path, Cost, KB).
+
+
+search([[Node,CurrentValue,Path]|_],CurrentPath,CurrentCost,_) :- goal(Node),CurrentPath=Path,CurrentCost=CurrentValue.
+search([[Node,CurrentValue,Path]|T],CurrentPath,CurrentCost,KB) :- findall([X,NewCost,[X|Path]],
+                                                                   (arc(Node,X,ArcCost,KB),NewCost is ArcCost+CurrentValue),
+                                                                   Children),
+                                                                   add_to_frontier(Children,T,New), search(New, CurrentPath,CurrentCost,KB).
+add_to_frontier([],Value,Value).
+add_to_frontier([H|T], Value, New) :- insert_sort([H|Value], Sorted), add_to_frontier(T, Sorted, New).
+
+% Edited version of insertion sort https://rosettacode.org/wiki/Sorting_algorithms/Insertion_sort#Prolog
+insert_sort(L1,L2) :- insert_sort_intern(L1,[],L2).
+ 
+insert_sort_intern([],L,L).
+insert_sort_intern([H|T],L1,L) :- insert(L1,H,L2),
+                                  insert_sort_intern(T,L2,L).
+insert([],X,[X]). 
+insert([H|T],X,[X,H|T]) :- less-than(X,H),
+                           !.
+insert([H|T],X,[H|T2]) :- insert(T,X,T2).
+
+
 
 
 
